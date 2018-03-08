ea83a20e93ad225d4a57f4e4d7af646bf57623eb
Alastair Gould <alastairgould@gmail.com>
2018-03-08 23:26:05 +0000

    Add validation and unit tests to publish to vsts usecase

--- TfsMigrate.Core/UseCases/PublishToVstsRepository/PublishToVstsGitRepositoryCommandHandler.cs
+++ TfsMigrate.Core/UseCases/PublishToVstsRepository/PublishToVstsGitRepositoryCommandHandler.cs
@@ -1,53 +1,50 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
-using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.SourceControl.WebApi;
 using TfsMigrate.Contracts;
 using TfsMigrate.Core.Exporter;
-using GitRepository = Microsoft.TeamFoundation.SourceControl.WebApi.GitRepository;
 
 namespace TfsMigrate.Core.UseCases.PublishToVstsRepository
 {
     public class PublishToVstsGitRepositoryCommandHandler : IRequestHandler<PublishToVstsGitRepositoryCommand, VstsGitRepository>
     {
-        public async Task<VstsGitRepository> Handle(PublishToVstsGitRepositoryCommand message, CancellationToken cancellationToken)
+        private readonly IValidator<PublishToVstsGitRepositoryCommand> _commandValidator;
+        private readonly ICreateVstsGitRepository _createVstsGitRepository;
+        private readonly Func<string, IGitClient> _createGitClient;
+
+        public PublishToVstsGitRepositoryCommandHandler(ICreateVstsGitRepository createVstsGitRepository,
+            Func<string, IGitClient> createGitClient)
         {
-            var gitRepository = await CreateVstsGitRepository(message, cancellationToken);
-            PushLocalRepositoryToVsts(message, gitRepository);
+            _commandValidator =  new PublishToVstsGitRepositoryCommandValidator();
+            _createVstsGitRepository = createVstsGitRepository;
+            _createGitClient = createGitClient;
+        }
+
+        public async Task<VstsGitRepository> Handle(PublishToVstsGitRepositoryCommand command, CancellationToken cancellationToken)
+        {
+            _commandValidator.ValidateAndThrow(command);
+
+            var gitRepository = await _createVstsGitRepository.CreateRepository(command.ProjectCollection,
+                command.TeamProject, command.RepositoryName, cancellationToken);
+
+            PushLocalRepositoryToVsts(command, gitRepository);
 
             return new VstsGitRepository()
             {
-                GitRepository = message.Repository,
-                ProjectCollection =  message.ProjectCollection,
-                RepositoryName = message.RepositoryName,
-                TeamProject = message.TeamProject
+                GitRepository = command.Repository,
+                ProjectCollection =  command.ProjectCollection,
+                RepositoryName = command.RepositoryName,
+                TeamProject = command.TeamProject
             };
         }
 
-        private static void PushLocalRepositoryToVsts(PublishToVstsGitRepositoryCommand message, GitRepository gitRepository)
+        private void PushLocalRepositoryToVsts(PublishToVstsGitRepositoryCommand message, string gitRepositoryUrl)
         {
-            var gitClient = new GitClient(message.Repository.Path);
-            gitClient.AddUpstreamRemote(gitRepository.RemoteUrl);
+            var gitClient = _createGitClient(message.Repository.Path);
+            gitClient.AddUpstreamRemote(gitRepositoryUrl);
             gitClient.PushAllUpstreamRemote();
-        }
-
-        private static async Task<GitRepository> CreateVstsGitRepository(PublishToVstsGitRepositoryCommand message,
-            CancellationToken cancellationToken)
-        {
-            var collection = new TfsTeamProjectCollection(message.ProjectCollection);
-            collection.EnsureAuthenticated();
-            var vstsGitApi = collection.GetClient<GitHttpClient>();
-
-            var gitRepository = new GitRepository()
-            {
-                Name = message.RepositoryName
-            };
-
-            return await vstsGitApi.CreateRepositoryAsync(
-                gitRepository,
-                message.TeamProject,
-                cancellationToken: cancellationToken);
         }
     }
 }
